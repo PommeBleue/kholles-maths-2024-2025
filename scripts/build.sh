308a13dd5ca696f9c94bb7b541c01176d74ffab9
Amar Ahmane <tiplouf@machine.local>
2024-10-12 23:23:10 +0200

    Error correction

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-xelatex -output-directory="./$1" "./${1}/main.tex"
+if [[ $2 == "" ]]; then 
+    xelatex -output-directory="./$1" "./${1}/main.tex"
+else
+    xelatex -output-directory="./$1" "./${1}/${2}.tex"
+fi
 
-rm -vf ./$1/*.log ./$1/*.aux ./$1/*.idx ./$1/*.toc ./$1/*.synctex.gz ./$1/*.out ./$1/*.dvi ./$1/*.fdb_latexmk ./$1/*.fls ./$1/*.ilg ./$1/*.ind
+rm -vf ./$1/*.log ./$1/*.aux ./$1/*.idx ./$1/*.toc ./$1/*.synctex.gz ./$1/*.out ./$1/*.dvi ./$1/*.fdb_latexmk ./$1/*.fls ./$1/*.ilg ./$1/*.ind ./$1/*.pgf-plot.gnuplot
