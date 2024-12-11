bb1b5901656cddcdc505a2fb779b938b7a555069
Amar Ahmane <tiplouf@machine.local>
2024-12-11 21:03:37 +0100

    Semaine 10

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,3 @@ if [[ $2 == "" ]]; then
 else
     xelatex -output-directory="./$1" "./${1}/${2}.tex"
 fi
-
-rm -vf ./$1/*.log ./$1/*.aux ./$1/*.idx ./$1/*.toc ./$1/*.synctex.gz ./$1/*.out ./$1/*.dvi ./$1/*.fdb_latexmk ./$1/*.fls ./$1/*.ilg ./$1/*.ind ./$1/*.pgf-plot.gnuplot
