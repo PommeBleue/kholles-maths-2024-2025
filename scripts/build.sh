0bcf71303382499b1c5cf7f6c64705b31934bc09
Amar Ahmane <tiplouf@machine.local>
2024-12-11 21:18:16 +0100

    Semaine 10

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+base_dir=$(cat ./scripts/base_dir)
+
 if [[ $2 == "" ]]; then 
     xelatex -output-directory="./$1" "./${1}/main.tex"
 else
     xelatex -output-directory="./$1" "./${1}/${2}.tex"
+    if [ ! -d $base_dir/$1 ]; then
+        mkdir $base_dir/$1
+    fi
+    cp ./$1/$2.pdf $base_dir/$1/$2.pdf
 fi
+
+git add .
