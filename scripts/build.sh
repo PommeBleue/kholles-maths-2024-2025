4f16ce9f2c98274317039110433d54e01bbc1190
Amar Ahmane <tiplouf@machine.local>
2024-12-29 03:00:09 +0100

    Updating build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 base_dir=$(cat ./scripts/base_dir)
+output_dir="./$1"
+tex_file="${2:-main}.tex"
+pdf_file="${2:-main}.pdf"
 
-if [[ $2 == "" ]]; then 
-    xelatex -output-directory="./$1" "./${1}/main.tex"
-else
-    xelatex -output-directory="./$1" "./${1}/${2}.tex"
-    if [ ! -d $base_dir/$1 ]; then
-        mkdir $base_dir/$1
-    fi
-    cp ./$1/$2.pdf $base_dir/$1/$2.pdf
+xelatex -output-directory="$output_dir" "$output_dir/$tex_file"
+
+if [ -n "$2" ]; then
+    target_dir="$base_dir/$1"
+    [ ! -d "$target_dir" ] && mkdir -p "$target_dir"
+    cp "$output_dir/$pdf_file" "$target_dir/$pdf_file"
 fi
 
 git add .
