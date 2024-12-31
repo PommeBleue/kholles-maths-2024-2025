7ff48267ec509f42439f1df663e0335fe14fcfe5
Amar Ahmane <tiplouf@machine.local>
2024-12-31 19:24:47 +0100

    Updated mystd.sty scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,13 +5,13 @@ output_dir="./$1"
 tex_file="${2:-main}.tex"
 pdf_file="${2:-main}.pdf"
 
-# xelatex -output-directory="$output_dir" "$output_dir/$tex_file"
+xelatex -output-directory="$output_dir" "$output_dir/$tex_file"
 
-# if [ -n "$2" ]; then
-#     target_dir="$base_dir/$1"
-#     [ ! -d "$target_dir" ] && mkdir -p "$target_dir"
-#     cp "$output_dir/$pdf_file" "$target_dir/$pdf_file"
-# fi
+if [ -n "$2" ]; then
+    target_dir="$base_dir/$1"
+    [ ! -d "$target_dir" ] && mkdir -p "$target_dir"
+    cp "$output_dir/$pdf_file" "$target_dir/$pdf_file"
+fi
 
 # Check if there are changes to commit
 if ! git diff-index --quiet HEAD --; then
