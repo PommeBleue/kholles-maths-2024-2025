f3ce65477083bcaef4f37dda79493fea227a525d
Amar Ahmane <tiplouf@machine.local>
2024-12-31 19:06:52 +0100

    Updated scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,18 +5,33 @@ output_dir="./$1"
 tex_file="${2:-main}.tex"
 pdf_file="${2:-main}.pdf"
 
-xelatex -output-directory="$output_dir" "$output_dir/$tex_file"
+# xelatex -output-directory="$output_dir" "$output_dir/$tex_file"
 
-if [ -n "$2" ]; then
-    target_dir="$base_dir/$1"
-    [ ! -d "$target_dir" ] && mkdir -p "$target_dir"
-    cp "$output_dir/$pdf_file" "$target_dir/$pdf_file"
-fi
+# if [ -n "$2" ]; then
+#     target_dir="$base_dir/$1"
+#     [ ! -d "$target_dir" ] && mkdir -p "$target_dir"
+#     cp "$output_dir/$pdf_file" "$target_dir/$pdf_file"
+# fi
 
 # Check if there are changes to commit
 if ! git diff-index --quiet HEAD --; then
     git add .
-    git commit -m "Adding $tex_file and $pdf_file"
+    created_files=$(git diff --name-only --diff-filter=A HEAD)
+    updated_files=$(git diff --name-only --diff-filter=M HEAD)
+    deleted_files=$(git diff --name-only --diff-filter=D HEAD)
+    commit_message_parts=()
+
+    echo $created_files
+    echo $updated_files
+    echo $deleted_files
+
+    [ -n "$created_files" ] && commit_message_parts+=("Added $(echo $created_files | tr '\n' ' ')")
+    [ -n "$updated_files" ] && commit_message_parts+=("Updated $(echo $updated_files | tr '\n' ' ')")
+    [ -n "$deleted_files" ] && commit_message_parts+=("Deleted $(echo $deleted_files | tr '\n' ' ')")
+
+    commit_message=$(IFS='; '; echo "${commit_message_parts[*]}")
+
+    git commit -m "$commit_message"
     git push origin main
 else
     echo "No changes to commit"
